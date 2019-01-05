608a9b7b9db7bf0a57ecbc1bafb8696a754601d3
raja-ashok <rashok.svks@gmail.com>
2019-01-05 13:18:38 +0530

    Use IFL Log Callback

--- src/ifl_tls12_client.c
+++ src/ifl_tls12_client.c
@@ -1,10 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "ifl.h"
 #include "ifl_tls12_client.h"
 
+#define IFL_LOG_FILE "./ifl_log.txt"
+
+FILE* g_ifl_log = NULL;
+
+void ifl_log_cb(uint8_t log_level, const char *log_msg)
+{
+    fwrite(log_msg, strlen(log_msg), 1, g_ifl_log);
+}
+
+void ifl_log_init()
+{
+    IFL_SetLogCB(ifl_log_cb);
+    g_ifl_log = fopen(IFL_LOG_FILE, "w");
+    if (!g_ifl_log) {
+        printf("Log file open failed\n");
+    }
+}
+
+void ifl_log_fini()
+{
+    fclose(g_ifl_log);
+    g_ifl_log = NULL;
+}
+
 int main()
 {
-    IFL *ifl;
+    IFL *ifl = NULL;
+    int ret_val = -1;
 
+    ifl_log_init();
     ifl = IFL_Init(IFL_CONF_CLIENT_HELLO_MSG, NULL);
     if (!ifl) {
         printf("IFL init failed\n");
@@ -13,8 +43,9 @@ int main()
 
     printf("IFL created successfully\n");
 
-    IFL_Fini(ifl);
-    return 0;
+    ret_val = 0;
 err:
-    return -1;
+    IFL_Fini(ifl);
+    ifl_log_fini();
+    return ret_val;
 }
